6152f5b34faae89dadffc5607ca6ed81297321f6
Daehee <sorhgid112@gmail.com>
2017-11-27 11:59:50 +0100

    added Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	render() {
+		return (
+			<div className="Footer">
+				<p>This is my own footer component.</p>
+			</div>
+		);
+	}
+}
+
+export default Footer;
